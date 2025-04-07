aa7cd3489e78dab3eed1d223a9f721768980cd39
Rifan Amrozi <rifan.amrozi@yahoo.com>
2025-04-07 11:02:03 +0700

    add menuview and update biodata view

--- sipApp/BiodataView.swift
+++ sipApp/BiodataView.swift
@@ -14,81 +14,195 @@ struct BiodataView: View {
     @State var selectedGender: String
     @State var fasting: Bool
     
+    @State var showModal = false
+    
     var body: some View {
         NavigationStack{
             VStack{
                 List{
-                    HStack{
-                        Text("Name")
-                            .foregroundColor(.gray)
-                        Spacer()
-                        VStack{
-                            TextField("Not Set", text: $name)
-                                .frame(width: 200, height: 10)
-                                .multilineTextAlignment(.trailing)
+                    Section{
+                        HStack{
+                            Text("Name")
                                 .foregroundColor(.gray)
+                            Spacer()
+                            VStack{
+                                TextField("Not Set", text: $name)
+                                    .frame(width: 200, height: 10)
+                                    .multilineTextAlignment(.trailing)
+                                    .foregroundColor(.gray)
+                            }
                         }
-                    }
-                    HStack{
-                        Text("Weight")
-                            .foregroundColor(.gray)
-                        Spacer()
-                        VStack{
-                            TextField("", text: $weight)
-                                .frame(width: 50, height: 10)
-                                .multilineTextAlignment(.trailing)
+                        HStack{
+                            Text("Weight")
                                 .foregroundColor(.gray)
+                            Spacer()
+                            VStack{
+                                TextField("", text: $weight)
+                                    .frame(width: 50, height: 10)
+                                    .multilineTextAlignment(.trailing)
+                                    .foregroundColor(.gray)
+                            }
+                            Text("kg")
                         }
-                        Text("kg")
-                    }
-                    HStack{
-                        Text("Age")
-                            .foregroundColor(.gray)
-                        Spacer()
-                        VStack{
-                            TextField("", text: $age)
-                                .frame(width: 200, height: 10)
-                                .multilineTextAlignment(.trailing)
+                        HStack{
+                            Text("Age")
                                 .foregroundColor(.gray)
+                            Spacer()
+                            VStack{
+                                TextField("", text: $age)
+                                    .frame(width: 200, height: 10)
+                                    .multilineTextAlignment(.trailing)
+                                    .foregroundColor(.gray)
+                            }
+                            Text("yrs")
                         }
-                        Text("yrs")
-                    }
-                    HStack{
-                        Text("Gender")
-                            .foregroundColor(.gray)
-                        Spacer()
-                        VStack{
-                            Picker("", selection: $selectedGender){
+                        HStack{
+                            Text("Gender")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            VStack{
+                                Picker("", selection: $selectedGender){
                                     Text("Male").tag("Male")
                                     Text("Female").tag("Female")
                                 }
-                            .multilineTextAlignment(.trailing)
-                            .foregroundColor(.gray)
-                            .frame(width: 200, height: 10)
-                            
+                                .multilineTextAlignment(.trailing)
+                                .foregroundColor(.gray)
+                                .frame(width: 200, height: 10)
+                                
+                            }
+                        }
+                        HStack{
+                            Text("Fasting")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Toggle("", isOn: $fasting)
+                                .toggleStyle(SwitchToggleStyle(tint: .cyan))
                         }
                     }
-                    HStack{
-                        Text("Fasting")
-                            .foregroundColor(.gray)
-                        Spacer()
-                        Toggle("", isOn: $fasting)
-                            .toggleStyle(SwitchToggleStyle(tint: .cyan))
+                    Section{
+                        HStack{
+                            Spacer()
+                            Button {
+                                showModal = true
+                            } label: {
+                                Text("Save")
+                                    .fontWeight(.bold)
+                                    .font(.callout)
+                                    .frame(width: 100, height: 45, alignment: .center)
+                                    .background(Color("WaterBlue"))
+                                    .cornerRadius(8)
+                                    .padding(.top, 330)
+                            }
+                            .sheet(isPresented: $showModal) {
+                                ZStack{
+                                    Color("WaterBlue")
+                                        .ignoresSafeArea()
+                                VStack {
+                                    VStack{
+                                        Text("Your daily water goal is")
+                                            .bold()
+                                            .foregroundStyle(Color.white)
+                                        Text("2600 mL") //Switch with Volume var
+                                            .fontWeight(.heavy)
+                                            .padding(.vertical)
+                                            .font(.system(size: 44))
+                                            .foregroundColor(Color.white)
+                                    }
+                                    HStack{
+                                        Spacer()
+                                        VStack{
+                                            Text("For every")
+                                                .bold()
+                                            VStack{
+                                                Text("⏰")
+                                                    .font(.system(size: 44))
+                                                    .padding(.bottom, 2)
+                                                Text("30 mins") //Switch with Interval var
+                                            }
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                            .frame(width: 140, height: 140)
+                                            .foregroundStyle(Color.black)
+                                            .background(Color.white)
+                                            .cornerRadius(10)
+                                            .multilineTextAlignment(.center)
+                                        }
+                                        Spacer()
+                                        Text("")
+                                            .frame(width: 1, height: 200)
+                                            .background(Color.white)
+                                        Spacer()
+                                        VStack{
+                                            Text("you have to drink")
+                                                .bold()
+                                            VStack{
+                                                Text("💧")
+                                                    .font(.system(size: 44))
+                                                    .padding(.bottom, 2)
+                                                Text("160 mL") //Switch with Interval var
+                                            }
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                            .frame(width: 140, height: 140)
+                                            .foregroundStyle(Color.black)
+                                            .background(Color.white)
+                                            .cornerRadius(10)
+                                            .multilineTextAlignment(.center)
+                                        }
+                                        Spacer()
+                                    }
+                                    VStack{
+                                        Text("That's equivalent to")
+                                            .padding(.top, 30)
+                                            .foregroundStyle(Color.white)
+                                        HStack{
+                                            Image("glassOfWater")
+                                            Text(" x 1.5")
+                                                .fontWeight(.heavy)
+                                                .padding(.vertical)
+                                                .font(.system(size: 44))
+                                                .foregroundColor(Color.white)
+                                        }
+                                        
+                                    }
+                                    
+                                    Button {
+                                        showModal = false
+                                    } label: {
+                                        Text("👍  Sip!")
+                                    }
+                                    .fontWeight(.bold)
+                                    .font(.title)
+                                    .frame(width: 150, height: 35)
+                                    .padding()
+                                    .background(Color.white)
+                                    .foregroundStyle(Color("WaterBlue"))
+                                    .cornerRadius(15)
+                                    .padding(.top, 50)
+                                    }
+                                }
+                            }
+                            Spacer()
+                        }
                     }
+                    .listRowBackground(Color("WaterBlue").opacity(0))
+                    .foregroundStyle(Color.white)
+                    
                 }
+                .background(Color("BackgroundYellow"))
+                .scrollContentBackground(.hidden)
                 .toolbar {
                     ToolbarItem(placement: .principal) {
                         Text("Biodata")
                             .font(.title)
-                            .foregroundColor(Color.black)
+                            .foregroundColor(Color("WaterBlue"))
                             .fontWeight(.heavy)
                             .padding(.top, 50)
                     }
                 }
-                
+
             }
         }
-        .background(.gray)
     }
 }
 
---
+++ sipApp/MenuView.swift
@@ -0,0 +1,57 @@
+//
+//  MenuView.swift
+//  sipApp
+//
+//  Created by Hany Wijaya on 26/03/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        NavigationStack{
+                ZStack{
+                    Color("BackgroundYellow")
+                        .ignoresSafeArea()
+                    VStack{
+                        VStack{
+                            Text("Biodata")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .padding(.bottom, 5)
+                                
+                            Text("Edit your personal information for a more up-to-date water intake goal")
+                        }
+                        .padding(40)
+                        .foregroundStyle(Color.white)
+                        .frame(width: 350, height: 300)
+                        .background(Color("WaterBlue"))
+                        .cornerRadius(15)
+                        .padding(.bottom, 30)
+                        .multilineTextAlignment(.center)
+                        
+                        VStack{
+                            Text("Preferences")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .padding(.bottom, 5)
+
+                            Text("Personalize your application and reminder")
+                        }
+                        .padding(40)
+                        .foregroundStyle(Color("WaterBlue"))
+                        .frame(width: 350, height: 300)
+                        .background(Color.white)
+                        .cornerRadius(15)
+                        .multilineTextAlignment(.center)
+                    }
+                    
+                }
+        }
+        
+    }
+}
+
+#Preview {
+    MenuView()
+}
